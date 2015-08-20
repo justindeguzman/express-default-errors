9c927b70dd97be58bb6d1c7f308d40ed1f75d101
Justin de Guzman <justin@justindeguzman.net>
2015-08-20 15:23:48 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# express-default-errors
+Default ExpressJS error-handling middleware.
+
+## Installation
+	$ npm install express-default-errors
+
+## Usage
+
+`express-default-errors` provides simple middleware to catch default errors and serve custom errors.
+
+```js
+var express = require('express')
+var errorHandler = require('express-default-errors')
+
+var app = express()
+var router = express.Router()
+
+// Valid route that will not cause any errors
+router.get('/', function (req, res, next) {
+  res.status(200)
+  res.send('success')
+})
+
+// To signify unauthorized access, simple create a new Error object with status = 401
+router.get('/restricted-route', function (req, res, next) {
+  var err = new Error()
+  err.status = 401
+  next(err)
+})
+
+// Custom error message
+router.get('/error-route', function (req, res, next) {
+  // Will default to err.status = 500 unless err.status is explicitly set
+  var err = new Error('A custom error was caused.')
+  next(err)
+})
+
+// This is not recommended, but you could also set non-standard status codes.
+// The default error message will be 'Internal Server Error' unless set.
+router.get('/invalid-status-code-error-route', function (req, res, next) {
+  var err = new Error()
+  err.status = 299
+  next(err)
+})
+
+// Any errors will be sent with the appropriate status code by the response object as:
+// {error: 'My error message here'}
+
+// The error handler should be used after all routers
+app.use(router)
+app.use(errorHandler())
+
+app.listen(3000)
+```
+
+## Default Errors
+
+Setting the status code will cause the middleware to serve default error messages when calling `new Error()` without passing in any arguments, or if `err.message` is not set.
+
+<table>
+	<tr><th>Status Code</th><th>Default Message</th></tr>
+	<tr><td>404</td><td><em>Not Found</em></td></tr>
+	<tr><td>401</td><td><em>Unauthorized</em></td></tr>
+	<tr><td>500</td><td><em>Internal Server Error</em></td></tr>
+</table>
+
+## License
+MIT
---
+++ test/index.js
@@ -0,0 +1,131 @@
+
+/* global describe it */
+
+/*
+ * Module dependencies.
+ */
+
+var express = require('express')
+var request = require('request')
+var should = require('should')
+
+var errorHandler = require('../lib')
+
+describe('express-default-errors', function () {
+
+  var port = 9999
+
+  /*
+   * Setup test server.
+   */
+
+  function setupTestServer () {
+    var app = express()
+
+    var router = express.Router()
+
+    router.get('/', function (req, res, next) {
+      res.status(200)
+      res.send('success')
+    })
+
+    router.get('/restricted-route', function (req, res, next) {
+      var err = new Error('Restricted route.')
+      err.status = 401
+      next(err)
+    })
+
+    router.get('/error-route', function (req, res, next) {
+      var err = new Error('A custom error was caused.')
+      next(err)
+    })
+
+    router.get('/invalid-status-code-error-route', function (req, res, next) {
+      var err = new Error()
+      err.status = 299
+      next(err)
+    })
+
+    app.use(router)
+    app.use(errorHandler())
+
+    return app.listen(port)
+  }
+
+  /*
+   * Tests.
+   */
+
+  it('should result in a 200 status code', function (done) {
+    var server = setupTestServer()
+    var url = 'http://localhost:' + port + '/'
+
+    request(url, function (err, response, body) {
+      should.not.exist(err)
+      should.exist(response)
+      should.exist(response.statusCode)
+      response.statusCode.should.eql(200)
+      server.close()
+      done()
+    })
+  })
+
+  it('should result in a 404 status code', function (done) {
+    var server = setupTestServer()
+    var url = 'http://localhost:' + port + '/nonexistant-route'
+
+    request(url, function (err, response, body) {
+      should.not.exist(err)
+      should.exist(response)
+      should.exist(response.statusCode)
+      JSON.parse(body).error.should.eql('Not Found')
+      response.statusCode.should.eql(404)
+      server.close()
+      done()
+    })
+  })
+
+  it('should result in a 401 status code', function (done) {
+    var server = setupTestServer()
+    var url = 'http://localhost:' + port + '/restricted-route'
+
+    request(url, function (err, response, body) {
+      should.not.exist(err)
+      should.exist(response)
+      should.exist(response.statusCode)
+      response.statusCode.should.eql(401)
+      server.close()
+      done()
+    })
+  })
+
+  it('should result in a 500 status code', function (done) {
+    var server = setupTestServer()
+    var url = 'http://localhost:' + port + '/error-route'
+
+    request(url, function (err, response, body) {
+      should.not.exist(err)
+      should.exist(response)
+      should.exist(response.statusCode)
+      JSON.parse(body).error.should.eql('A custom error was caused.')
+      response.statusCode.should.eql(500)
+      server.close()
+      done()
+    })
+  })
+
+  it('should result in a 299 status code', function (done) {
+    var server = setupTestServer()
+    var url = 'http://localhost:' + port + '/invalid-status-code-error-route'
+
+    request(url, function (err, response, body) {
+      should.not.exist(err)
+      should.exist(response)
+      should.exist(response.statusCode)
+      JSON.parse(body).error.should.eql('Internal Server Error')
+      response.statusCode.should.eql(299)
+      server.close()
+      done()
+    })
+  })
+})
